9ea1b422ab54852b7156a1babe492dadcfd1710c
Ctrl-S <j1459.j1459@gmail.com>
2019-02-20 20:38:36 +0800

    Initial commit. Starting to lay out what needs doing. Starting to figure out how to manage a workflow for this on ubuntu.

---
+++ main.py
@@ -0,0 +1,39 @@
+# Main.py
+# Playground for now, later it can be something 
+
+
+# Load a thread from the API
+
+
+
+
+
+# We want to pull threads from the API and store them
+# We want to compare the latest thread data to the saved thread data
+# We want to compare posts
+# We want to keep track of what images we've saved
+# We want to save images we don't have
+
+
+
+# Long term datastructures:
+# Postgres DB?
+# Fields:
+# Banned: "This shit will not touch my server, under any circumstances."
+# Deleted: "Nobodies eyes may glimpse this."
+
+# TABLE %BOARD%_threads
+# int thread_num, JSONB posts, boolean deleted, boolean banned,
+
+# TABLE %BOARD%_media
+# unicode disk_path_full, disk_path_thumb, disk_path_opthumb, unicode md5b64, unicode sha1b64, unicode sha512b64, int size, boolean deleted, boolean banned,
+
+
+# TABLE %BOARD%_thumbs
+
+# TABLE %BOARD%_deleted
+# unicode md5b64, unicode sha1b64, unicode sha512b64, int size, unicode reason, unicode deleted_by, 
+
+# TABLE global_media
+
+# TABLE global_thumbs
---
+++ readme.md
@@ -0,0 +1,23 @@
+# FutaWilly
+A firm and sturdy infinichan archiver.
+(Because FutaBilly was just too hard for me to figure out how to install.)
+This is a tool to archive boards from an infinichan installation.
+
+# Installation
+TODO WRITEME
+(This should be enough that a literal child moron can get it running)
+
+# Configuration
+TODO WRITEME
+(This should be enough that a literal child moron can get it running)
+
+# Contributing
+TODO WRITEME
+
+# Licence
+TODO WRITEME
+No license is given.
+Just archive shit and we can care about licensing later.
+
+
+
